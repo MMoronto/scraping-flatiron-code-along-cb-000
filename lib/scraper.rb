0ae172671ca305a42e736e4f3bec451455006e6f
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-06-12 19:55:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,8 +15,9 @@ class Scraper
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
       
-      binding.pry 
+      #binding.pry 
     end
+      binding.pry
   end
 
   
