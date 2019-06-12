052be1e9000a1bcc8935eb1e742744ad011ebdde
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-06-12 16:09:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,16 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
+  
   def get_page
-    doc = Nokogiri: :HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-  end 
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
+   binding.pry 
+  end
   
     def print_courses
     self.make_courses
@@ -20,6 +24,7 @@ class Scraper
   end
   
 end
+Scraper.new.get_page 
 
 
 
